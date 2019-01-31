1f119060c6496eac0f9fcafd88b128007a1dbcc0
David Bogaerts <dbogaerts@gmail.com>
2019-01-30 22:37:37 -0600

    all edit routes fixed and functional

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -3,9 +3,9 @@ const router   = express.Router();
 const Project  = require('../models/project.js');
 const User     = require('../models/user.js');
 
-//index route
+//index route - loads TOC for all projects
 
-router.get('/', async (req, res) => {
+router.get('/', async (req, res, next) => {
   try {
     const publishedProjects = await Project.find({publish: true});
     const foundUser = await User.findById(req.session.userId);
@@ -37,9 +37,9 @@ router.get('/', async (req, res) => {
   }
 })
 
-//new route
+//new route - loads new.ejs which allows instantiation of new project
 
-router.get('/new', async (req, res) => {
+router.get('/new', async (req, res, next) => {
   try {
     res.render('project/new.ejs');
         
@@ -50,15 +50,13 @@ router.get('/new', async (req, res) => {
   }
 })
 
-//new-post route
-
-router.post('/', async (req, res) => {
+//new-post route -- this instantiates a new project to the server with a project name
+//after instantiation, route loads new-content.ejs, 
+//which will allow updates to project
+router.post('/', async (req, res, next) => {
   try {
 
     const newProject = await Project.create(req.body);
-    console.log('+++++++++++++++++++++++++++++++++++++');
-    console.log(newProject, ' this is NEWPROJECT')
-    console.log('+++++++++++++++++++++++++++++++++++++');
     res.render('project/new-content.ejs', {
       project: newProject
     });
@@ -72,11 +70,11 @@ router.post('/', async (req, res) => {
 
 //random route -- custom for random project link
 
-router.get('/random', async (req, res) => {
+router.get('/random', async (req, res, next) => {
   try {
     const publishedProjects = await Project.find({publish: true});
     const foundUser = await User.findById(req.session.userId);
-    const randomProject = publishedProjects[Math.floor(Math.random() * publishedProjects.length)];
+    const randomProject = await publishedProjects[Math.floor(Math.random() * publishedProjects.length)];
     res.render('project/show.ejs', {
       project: randomProject,
       user: foundUser
@@ -90,8 +88,9 @@ router.get('/random', async (req, res) => {
 })
 
 // ------------ Project Show ------------- //
+// shows completed project
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', async (req, res, next) => {
   try {
     console.log(req.session, '<----- project show req.session');
     const foundProject = await Project.findById(req.params.id);
@@ -108,16 +107,18 @@ router.get('/:id', async (req, res) => {
   }
 })
 
-//put routes -- for creating how-to project
+//put routes -- used to add new content to in progress project
 
-router.put('/:id', async (req, res) => {
+router.put('/:id', async (req, res, next) => {
   try {
     const foundProject = await Project.findById(req.params.id);
     const foundUser = await User.findById(req.session.userId);
-    
+    console.log(foundProject, ' foundProject BEFORE pushes');
     foundProject.text.push(req.body.text);
     foundProject.media.push(req.body.media);
+    console.log(foundProject, ' foundProject AFTER pushes but BEFORE SAVE');
     foundProject.save();
+    console.log(foundProject, ' foundProject AFTER SAVE');
     
     res.render('project/new-content.ejs', {
       project: foundProject,
@@ -131,9 +132,9 @@ router.put('/:id', async (req, res) => {
   }
 })
 
-//delete project route
+//delete project route -- NOTE: Make sure it deletes everywhere
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', async (req, res, next) => {
   try {
     console.log(req.params.id);
     const foundProject = await Project.findByIdAndRemove(req.params.id);
@@ -150,16 +151,24 @@ router.delete('/:id', async (req, res) => {
   }
 })
 
-router.put('/:id/publish', async (req, res) => {
+// Update route -- PUBLISH - when project is built, this adds
+// {publish: true} to the project so that it registers with the site
+// as complete.
+
+router.put('/:id/publish', async (req, res, next) => {
   try {
     const foundProject = await Project.findById(req.params.id);
     const foundUser = await User.findById(req.session.userId);
-    foundProject.publish = req.body.publish;
-    foundProject.save();
-    console.log(foundProject, ' <--- foundProject');
-    foundUser.projects.push(foundProject);
-    foundUser.save();
-    console.log(foundUser, ' <--- founduser here -----');
+    if (foundProject.publish) {
+      foundUser.projects.id(req.params.id).remove();
+      foundUser.projects.push(foundProject);
+      foundUser.save();
+    } else {
+      foundProject.publish = req.body.publish;
+      foundProject.save();
+      foundUser.projects.push(foundProject);
+      foundUser.save();
+    }
     res.redirect('/project');
         
   } catch (err) {
@@ -169,7 +178,9 @@ router.put('/:id/publish', async (req, res) => {
   }
 })
 
-router.put('/:id/edit', async (req, res) => {
+//edit route -- triggered by pressing Edit in the My Account area
+
+router.put('/:id/edit', async (req, res, next) => {
   try {
     const foundProject = await Project.findById(req.params.id);
     res.render('project/edit.ejs', {
@@ -183,12 +194,15 @@ router.put('/:id/edit', async (req, res) => {
   }
 })
 
+//edit route - triggered by pressing edit on specific frame of project
+//in preview pane. 
+
 router.post('/:id/edit-target', async (req, res, next) => {
   try {
     const foundProject = await Project.findById(req.body.proj_id);
     const parsedIdx = parseInt(req.body.index, 10);
 
-    res.render('project/edit.ejs', {
+    res.render('project/edit-targeted.ejs', {
       project: foundProject,
       arrId: parsedIdx
     })
@@ -200,11 +214,26 @@ router.post('/:id/edit-target', async (req, res, next) => {
   }
 })
 
-router.put('/:id/update', async (req, res) => {
+//update route
+
+router.put('/:id/update', async (req, res, next) => {
   try {
-    //this is when we are done making changes
-    //and splicing back in
-    console.log('UPDATE ROUTE HIT')
+    console.log(req.body);
+
+    const foundProject = await Project.findById(req.body.proj_id);
+    const foundUser = await User.findById(req.session.userId);
+
+    foundProject.media.splice(req.body.index, 1, req.body.media);
+    foundProject.text.splice(req.body.index, 1, req.body.text);
+    foundProject.save();
+
+    foundUser.projects.id(req.body.proj_id).remove();
+    foundUser.projects.push(foundProject);
+    foundUser.save();
+
+    res.render('project/new-content.ejs', {
+      project: foundProject
+    })
         
   } catch (err) {
     console.log(err);
